c8f9b5fd5dbde8962391ef2cdb4a9a371ed48d07
soutam <soutampaul@gmail.com>
2019-09-03 11:01:58 +0530

    Query DB with role name

--- src/main/java/com/payroll/microservice/RollMicroservice/Repository/RoleServiceRepository.java
+++ src/main/java/com/payroll/microservice/RollMicroservice/Repository/RoleServiceRepository.java
@@ -3,5 +3,9 @@ package com.payroll.microservice.RollMicroservice.Repository;
 import com.payroll.microservice.RollMicroservice.Model.RoleDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleServiceRepository extends JpaRepository<RoleDetails, Long> {
+
+    public Optional<RoleDetails> findByRoleName(String roleName);
 }
